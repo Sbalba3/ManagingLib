21227c0ea7e88b465c46b262a4cbff3f794b4a5c
Sbalba3 <137001608+Sbalba3@users.noreply.github.com>
2023-10-21 12:09:04 +0300

    now i make generic interface to contain five method of our crud operation and one method to save

---
+++ ManagingLib.BLL/Interfaces/IGenericRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaingLib.BLL.Interfaces
+{
+    public interface IGenericRepo<T>where T : class 
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task Add(T item);
+        void Update(T item);
+        void Delete(T item);
+        Task Save();
+    }
+}
